e70c0fada19e01d0d51dc15b1b1dada5a4252908
niflheims <niflheims@gmail.com>
2024-02-18 17:58:11 +0100

    basic monitoring with CPU and MEM, tested under Win11

--- gometrics.go
+++ gometrics.go
@@ -1,47 +1,47 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"strings"
 
 	"github.com/Nibelheims/gometrics/pkg/keyboard"
 	"github.com/Nibelheims/gometrics/pkg/monitoring"
 	"github.com/karalabe/hid"
 )
 
+func getHID(k *keyboard.Keyboard) (*hid.DeviceInfo, error) {
+	hids := hid.Enumerate(keyboard.Lily58.VendorID, keyboard.Lily58.ProductID)
+	if hids == nil {
+		return nil, errors.New("failed to enumerate HID devices")
+	}
+	for _, hid := range hids {
+		if hid.UsagePage == keyboard.Lily58.UsagePage &&
+			hid.Usage == keyboard.Lily58.UsageID {
+			return &hid, nil
+		}
+	}
+	return nil, errors.New("Could not find the device \"" + k.Name + "\"")
+}
+
 func main() {
-	// Enumerate all the HID devices in alphabetical path order
 	if !hid.Supported() {
 		fmt.Println("HID lib not supported on this platform, exiting")
 		return
 	}
-	hids := hid.Enumerate(keyboard.Lily58.VendorID, keyboard.Lily58.ProductID)
-	if hids == nil {
-		panic("failed to enumerate HID devices")
+
+	keebInfo, err := getHID(&keyboard.Lily58)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
-	for i := 0; i < len(hids); i++ {
-		for j := i + 1; j < len(hids); j++ {
-			if hids[i].Path > hids[j].Path {
-				hids[i], hids[j] = hids[j], hids[i]
-			}
-		}
+
+	keeb, err := keebInfo.Open()
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
-	fmt.Printf("hid.Supported() %v\n", hid.Supported())
-	for i, hid := range hids {
-		fmt.Println(strings.Repeat("-", 128))
-		fmt.Printf("HID #%d\n", i)
-		fmt.Printf("  OS Path:      %s\n", hid.Path)
-		fmt.Printf("  Vendor ID:    %#04x\n", hid.VendorID)
-		fmt.Printf("  Product ID:   %#04x\n", hid.ProductID)
-		fmt.Printf("  Release:      %d\n", hid.Release)
-		fmt.Printf("  Serial:       %s\n", hid.Serial)
-		fmt.Printf("  Manufacturer: %s\n", hid.Manufacturer)
-		fmt.Printf("  Product:      %s\n", hid.Product)
-		fmt.Printf("  Usage Page:   %#04x\n", hid.UsagePage)
-		fmt.Printf("  Usage:        %d\n", hid.Usage)
-		fmt.Printf("  Interface:    %d\n", hid.Interface)
-	}
-	fmt.Println(strings.Repeat("=", 128))
+	defer keeb.Close()
+	fmt.Println("Successfully opened  \"" + keyboard.Lily58.Name + "\"")
 
 	m := monitoring.NewMonitor(500)
 	m.Run()
@@ -49,6 +49,9 @@ func main() {
 	i := 0
 	for u := range m.C() {
 		fmt.Printf("cpu: %f%%\tmem: %f%%\n", u.CpuPercent, u.MemPercent)
+		buffer := keyboard.UsageToHIDReport(u)
+		//fmt.Println(buffer)
+		keeb.Write(buffer)
 		i++
 		if i > 10 {
 			m.Stop()
--- pkg/keyboard/keyboard.go
+++ pkg/keyboard/keyboard.go
@@ -1,5 +1,9 @@
 package keyboard
 
+import (
+	"github.com/Nibelheims/gometrics/pkg/monitoring"
+)
+
 type Keyboard struct {
 	VendorID  uint16
 	ProductID uint16
@@ -14,4 +18,17 @@ var Lily58 = Keyboard{
 	0xFF60,
 	0x0061,
 	"Lily58",
+}
+
+// gometrics
+var MAGIC = []byte{0x67, 0x6F, 0x6D, 0x65, 0x74, 0x72, 0x69, 0x63, 0x73}
+
+func UsageToHIDReport(u monitoring.Usage) []byte {
+	b := make([]byte, len(MAGIC)+2)
+
+	copy(b[0:len(MAGIC)], MAGIC)
+	copy(b[len(MAGIC)+0:len(MAGIC)+1], []byte{uint8(u.CpuPercent)})
+	copy(b[len(MAGIC)+1:len(MAGIC)+2], []byte{uint8(u.MemPercent)})
+
+	return b
 }
